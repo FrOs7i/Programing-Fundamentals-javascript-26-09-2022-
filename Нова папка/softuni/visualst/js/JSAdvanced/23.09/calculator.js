f01c90b4973d5d756ef4eb38eb57fffe33ea8af7
FrOs7i <114285250+FrOs7i@users.noreply.github.com>
2022-10-06 21:31:21 +0100

    AS

--- Нова папка/softuni/visualst/js/JSAdvanced/23.09/calculator.js
+++ Нова папка/softuni/visualst/js/JSAdvanced/23.09/calculator.js
@@ -1,7 +1,7 @@
 function calculator(num1,operator,num2) {
     let num1ToString=num1.toString()
     let num2ToString=num2.toString()
-    let mainString = `${num1ToString}  ${operator} + ${num2ToString}`
+    let mainString = `${num1ToString}  ${operator}  ${num2ToString}`
     console.log(eval(mainString).toFixed(2))
 }
 calculator(5,"+",100)
--- Нова папка/softuni/visualst/js/JSAdvanced/23.09/gladiatorExpenses.js
+++ Нова папка/softuni/visualst/js/JSAdvanced/23.09/gladiatorExpenses.js
@@ -2,7 +2,7 @@ function gladiatorExpenses(lostFights, helmetPr, swordPr, shieldPr, armorPr) {
     let tottalPrice = 0
 let shieldCount=0
     for (let index = 1; index <= lostFights; index++) {
-        if (index % 2 == 0 || index % 3 === 0) {
+        
             if (index % 2 == 0) {
                 tottalPrice += helmetPr
             }
@@ -19,7 +19,7 @@ let shieldCount=0
             }
 
 
-        }
+        
     }
     console.log(`Gladiator expenses: ${tottalPrice.toFixed(2)} aureus`);
 }
